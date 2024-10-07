466f06762459c50be3920997e80414555d63f964
Anston Sorensen <ansorensen1118@gmail.com>
2024-10-07 17:55:58 -0500

    Fix package name

--- src/main/java/com/acclash/samplenmsplugin/SampleNMSPlugin.java
+++ src/main/java/com/acclash/samplenmsplugin/SampleNMSPlugin.java
@@ -14,7 +14,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
-package com.acclash.sampleNMSPlugin;
+package com.acclash.samplenmsplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
